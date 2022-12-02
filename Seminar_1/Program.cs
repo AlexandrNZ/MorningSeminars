9a9ac196764e37a115b1cd934d74fafeb264563f
Alexandr <A.N.Zemchenkov@gmail.com>
2022-12-02 16:20:13 +0300

    commit 1

--- Seminar_1/Program.cs
+++ Seminar_1/Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// int a = 5; // число
+// double b = 2.5; // вещественное число
+// bool c = true; // логический тип
+// bool v = false;
+
+// string d = "world" // строки
+// char e = '%' // символ
+// $ перед ковычками, вместо конкотинации, а переменную в {}
+// WriteLine вывод данных
+// ReadLine ввод данных
+
+// Console.WriteLine("My number is" + a);
+
+// Convert.ToInt32 конвертация в инт (32 стандарт)
+// Convert.ToInt16 
+// Convert.ToInt64
+/*
+закоментировать 
+участок
+кода
+*/
+/*
+Console.Write("Input a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+*/
+/*
+Console.Write("Input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second namber: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int quad = num2 * num2;
+if(num1 == quad)
+{
+    Console.WriteLine("Yes! ");
+}
+else
+{
+    Console.WriteLine("No! ");
+}
+*/
+
+/*
+Console.Write("Input an integer number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current = -num;
+
+while(current <= num)
+{
+    Console.Write(current + " ");
+    current++;
+}
+
+
+12345 / 10 = 1234
+12345 / 100 = 123
+12345 / 1000 = 12
+12345 / 10000 = 1
+
+12345 % 10 = 5
+12345 % 100 = 45
+12345 % 1000 = 345
+12345 % 10000 = 2345
+12345 % 100000 = 12345
+*/
